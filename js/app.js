a95333345691f36c50c055ba4d898ce3db05d8e0
Jaime Alvarez <jealvarez123@gmail.com>
2022-03-23 14:09:00 -0500

    First commit

---
+++ js/app.js
@@ -0,0 +1,6 @@
+userName = 'Pepe hands';
+
+// example of ternary expression
+// day === "Monday" ? price -= 1.5 : price += 1.5;
+
+userName === 'Pepe hands' ? console.log(`Hello, ${userName}!`) : console.log('Hello!');
