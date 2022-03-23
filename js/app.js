ae0a96bc38d6f60888779d26a75f544cecec7eab
Jaime Alvarez <jealvarez123@gmail.com>
2022-03-23 15:40:18 -0500

    I added the rest of the code

--- js/app.js
+++ js/app.js
@@ -4,3 +4,41 @@ userName = 'Pepe hands';
 // day === "Monday" ? price -= 1.5 : price += 1.5;
 
 userName === 'Pepe hands' ? console.log(`Hello, ${userName}!`) : console.log('Hello!');
+
+const userQuestion = 'will there be a tomorrow?';
+
+console.log(`${userName} asked ${userQuestion}`);
+
+let randomNumber = Math.floor(Math.random() * 8);
+
+let eightBall = '';
+
+
+switch (randomNumber) {
+  case 0:
+    eightBall = 'It is certain';
+    break;
+  case 1:
+    eightBall = 'It is decidedly so';
+    break;
+  case 2:
+    eightBall = 'Reply hazy try again';
+    break;
+  case 3:
+    eightBall = 'Cannot predict now';
+    break;
+  case 4:
+    eightBall = 'Do not count on it';
+    break;
+  case 5:
+    eightBall = 'My sources say no';
+    break;
+  case 6:
+    eightBall = 'Outlook not so good';
+    break;
+  case 7:
+    eightBall = 'Signs point to yes';
+    break;
+
+}
+console.log(`The 8 Ball says ${eightBall}`)
